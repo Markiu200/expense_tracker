cd7c6920718edc596afc7040717b569e72eab379
markiu200 <markiu2007@interia.pl>
2025-05-09 17:18:12 +0200

    Add category field (with Category enum) to Expense model
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37143060#overview

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,12 +1,20 @@
 import 'package:expense_tracker/models/compound_int.dart';
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   static int lastId = 0;
   static int nextId() => lastId++;
 
-  Expense({required this.title, required this.amount, required this.date});
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  });
   final int id = Expense.nextId();
   final String title;
   final CompoundInt amount;
   final DateTime date;
+  final Category category;
 }
