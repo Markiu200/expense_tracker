b4566f8c71d6e0a9f999f22f369b703c46a2af91
markiu200 <markiu2007@interia.pl>
2025-05-16 18:29:27 +0200

    Add SnackBar showing message on remove and allowing to undo that action
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37143154#overview

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -36,9 +36,24 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final int expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        action: SnackBarAction(
+          label: "Undo",
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+        content: const Text('Expense deleted.'),
+      ),
+    );
   }
 
   void _openAddExpenseOverlay() {
