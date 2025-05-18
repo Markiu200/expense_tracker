4c46e01155b48244025525b3c29f61711a8293ff
markiu200 <markiu2007@interia.pl>
2025-05-18 20:37:54 +0200

    Add dark color scheme
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37143186#overview

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,28 @@ ColorScheme kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+ColorScheme kDarkColorScheme = ColorScheme.fromSeed(
+  // brightness tells Flutter for what purpose should this ColorScheme be adjusted
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
@@ -31,6 +50,7 @@ void main() {
           ),
         ),
       ),
+      themeMode: ThemeMode.system, // default
       home: Expenses(),
     ),
   );
